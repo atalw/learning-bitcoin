dda10660344b83d653a6fef0dc4a0de972f1fdae
Akshit Talwar <akshitalwar11@gmail.com>
2022-04-19 11:25:29 +0530

    moved stuff around

--- bitcoin/transaction/src/main.rs
+++ bitcoin/transaction/src/main.rs
@@ -1,13 +1,10 @@
-use std::error::Error;
-use std::io::Cursor;
+#![allow(dead_code)]
 
-use serde_json::Value;
-
+mod deserialize;
 mod txio;
 
 #[derive(Debug)]
-#[allow(dead_code)]
-struct Transaction {
+pub struct Transaction {
 	version: u32,
 	flag: Option<u16>,
 	in_counter: u64, // varint -> byte size 1-9
@@ -19,8 +16,7 @@ struct Transaction {
 }
 
 #[derive(Debug, Clone)]
-#[allow(dead_code)]
-struct Input {
+pub struct Input {
 	previous_tx: String,
 	tx_index: u32,
 	script_sig: String,
@@ -29,141 +25,25 @@ struct Input {
 }
 
 #[derive(Debug, Clone)]
-#[allow(dead_code)]
-struct Output {
+pub struct Output {
 	amount: u64,
 	script_pub_key: String,
 }
 
 #[derive(Debug, Clone)]
-#[allow(dead_code)]
-struct ExtraInfo {
+pub struct ExtraInfo {
 	miner_fee: u64,
 	tx_size: u64,
 }
 
 fn main() {
 	for raw_transaction in get_raw_transactions() {
-		match parse_raw_data(raw_transaction) {
+		match deserialize::parse_raw_data(raw_transaction) {
 			Ok(transaction) => { println!("{:#?}", transaction) },
 			Err(e) => panic!("{}", e)
 		}
 		break
 	}
-}
-
-fn parse_raw_data(data: String) -> Result<Transaction, Box<dyn Error>> {
-	let d: Value = serde_json::from_str(&data)?;
-	println!("raw transaction: {}", d["result"]);
-	println!("-------------------");
-
-	// convert to bytes
-	let result: Vec<u8> = txio::decode_hex(d["result"].as_str().unwrap())?;
-	let mut stream = Cursor::new(result.clone());
-
-	// version: always 4 bytes long
-	let version = txio::read_u32_le(&mut stream);
-
-	// optional, always 0001 if present
-	let mut flag = Some(txio::read_u16_be(&mut stream));
-	if flag != Some(1) {
-		txio::unread(&mut stream, -2);
-		flag = None
-	}
-
-	// number of inputs
-	let in_counter = txio::read_compact_size(&mut stream);
-
-	let mut inputs: Vec<Input> = Vec::new();
-	for _ in 0..in_counter {
-
-		let previous_tx = txio::read_hex256_le(&mut stream);
-		let tx_index = txio::read_u32_le(&mut stream);
-		// question: why are there n extra bytes in script_sig? in/out_script_length specifies it
-		let in_script_length = txio::read_compact_size(&mut stream);
-		let script_sig= txio::read_hex_var_be(&mut stream, in_script_length);
-		let sequence = txio::read_hex32_le(&mut stream);
-		let prevout = match get_prevout(&previous_tx, tx_index) {
-			Ok(output) => output,
-			Err(e) => panic!("{}", e)
-		};
-
-		let input = Input {
-			previous_tx,
-			tx_index,
-			script_sig,
-			sequence,
-			prevout
-		};
-
-		inputs.push(input);
-	}
-
-
-	// number of outputs
-	let out_counter = txio::read_compact_size(&mut stream);
-	assert_ne!(out_counter, 0);
-
-	let mut outputs: Vec<Output> = Vec::new();
-	for _ in 0..out_counter {
-
-		let amount = txio::read_u64_le(&mut stream);
-		let out_script_length = txio::read_compact_size(&mut stream);
-		let script_pub_key = txio::read_hex_var_be(&mut stream, out_script_length);
-
-		let output = Output {
-			amount,
-			script_pub_key,
-		};
-
-		outputs.push(output);
-	}
-
-	// List of witnesses
-	if flag.is_some() {}
-
-	// always 4 bytes long
-	let lock_time = txio::read_u32_le(&mut stream);
-
-	let total_input_amount = inputs.iter().fold(0, |acc, x| acc + x.prevout.amount);
-	let total_output_amount = outputs.iter().fold(0, |acc, x| acc + x.amount);
-	assert!(total_output_amount <= total_input_amount);
-	let miner_fee = total_input_amount - total_output_amount;
-
-	let tx_size = stream.position();
-
-
-	let extra_info = ExtraInfo { 
-		miner_fee,
-		tx_size 
-	};
-
-	let transaction = Transaction {
-		version,
-		flag,
-		in_counter,
-		inputs,
-		out_counter,
-		outputs,
-		lock_time,
-		extra_info,
-	};
-
-	Ok(transaction)
-}
-
-fn get_prevout(previous_tx: &str, index: u32) -> Result<Output, Box<dyn Error>> {
-	let endpoint = "https://blockstream.info/api/tx/".to_string() + previous_tx;
-	let response = reqwest::blocking::get(endpoint)?.json::<serde_json::Value>()?;
-
-	let prevouts = response["vout"].as_array().unwrap();
-
-	let output = Output {
-		amount: prevouts[index as usize]["value"].as_u64().unwrap_or(0),
-		script_pub_key: prevouts[index as usize]["scriptpubkey"].to_string().replace("\"", ""),
-	};
-
-	Ok(output)
 }
 
 fn get_raw_transactions() -> Vec<String> {
